216bc31a84f6c00db9171a7e154da2a13d7f7ec4
fletchan <admin@fletchan.com>
2017-04-24 16:00:11 -0500

    Add tmux theme

---
+++ tmux/theme.sh
@@ -0,0 +1,31 @@
+#### Tmux theme configuration
+
+tm_icon="🐧"
+tm_color_active=colour51
+tm_color_inactive=colour241
+tm_color_feature=colour198
+
+tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
+tm_date="#[fg=$tm_color_inactive] %R %d %b"
+tm_host="#[fg=$tm_color_feature,bold]#h"
+
+# statusbar settings and colors
+set-option -g status-style fg=$tm_color_active
+set -g status-left $tm_session_name' '
+set -g status-right $tm_date' '$tm_host
+
+# active window colors
+set-window-option -g window-status-current-style fg=$tm_color_active
+set-window-option -g window-status-current-format "#[bold]#I #W"
+
+# pane border colors
+set-window-option -g pane-border-style fg=$tm_color_inactive
+set-window-option -g pane-active-border-style fg=$tm_color_active
+
+# message text colors
+set-option -g message-style fg=$tm_color_active
+
+# pane number display colors
+set-option -g display-panes-active-colour $tm_color_active
+set-option -g display-panes-colour $tm_color_inactive
+
